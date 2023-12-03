0db67fcd67edcaeae6d410ddea4d00676b90f823
Brcasp00 <145512742+Brcasp00@users.noreply.github.com>
2023-12-02 21:11:54 -0300

    Update Calcular.js

--- Calcular.js
+++ Calcular.js
@@ -1,16 +1,36 @@
-export default class Calcular{
-    constructor(saldo, data){
-        this.saldo = parseFloat(saldo)
-        this.data = data
-        this.credito = 0
-
-
+export default class Calcular {
+    constructor(saldo, data, porcentagem) {
+        this.data = data;
+        this.credito = 0;
+        this.saldo = saldo;
+        this.debito = 0;
+        this.chequeespecial = 2640;// o salario minimo ta 1320 e o cheque especial sao dois
+        this.porcentagem = porcentagem;
+        this.poupanca = 0;
     }
 
+    adicionarCredito(num) {
+        num = parseFloat(num)
+        var soma = num + this.saldo
+        this.poupanca = soma * (this.porcentagem / 100)
+        this.saldo = soma - this.poupanca
+        this.credito = num
+        
+    }
 
-    adicionar_credito(valor){
-        valor = parseFloat(valor)
-        this.credito += valor
-        return "crédito adicionado de "+  this.credito + " na data de: " + this.data
+    debitar(num) {
+        num = parseFloat(num)
+        var sub = this.saldo - num
+        if (num > this.saldo) {
+            this.chequeespecial = this.chequeespecial + sub
+        }else{
+            this.chequeespecial = 2640
+        }
+        
+        this.saldo = sub
+        this.debito = num
+
+        
+        
     }
 }
